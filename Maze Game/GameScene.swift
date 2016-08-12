67646db223a95714d1e5cff151dd2708287c81b8
Jorge Casariego <jorgekasa@gmail.com>
2016-08-12 17:45:23 -0400

    Node contact implemented showing an Alert

--- Maze Game/GameScene.swift
+++ Maze Game/GameScene.swift
@@ -9,9 +9,48 @@
 import SpriteKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    let manager = CMMotionManager()
+    var player = SKSpriteNode()
+    var endNode = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
-       
+        
+        self.physicsWorld.contactDelegate = self
+        
+        player = self.childNodeWithName("player") as! SKSpriteNode
+        endNode = self.childNodeWithName("endNode") as! SKSpriteNode
+        
+        manager.startAccelerometerUpdates()
+        manager.accelerometerUpdateInterval = 0.1
+        manager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()){
+            (data, error) in
+            
+            self.physicsWorld.gravity = CGVectorMake(CGFloat((data?.acceleration.x)! * 10), CGFloat((data?.acceleration.y)!) * 10)
+        }
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        let bodyA = contact.bodyA
+        let bodyB = contact.bodyB
+        
+        if bodyA.categoryBitMask == 1 && bodyB.categoryBitMask == 2 || bodyA.categoryBitMask == 2 && bodyB.categoryBitMask == 1 {
+            
+            showAlert(title: "Maze Game", message: "You Won!")
+            
+        }
+        
+    
+    }
+    
+    func showAlert(title title: String, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .Cancel) { _ in }
+        alertController.addAction(okAction)
+        
+        self.view?.window?.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
     }
     
     override func update(currentTime: CFTimeInterval) {
